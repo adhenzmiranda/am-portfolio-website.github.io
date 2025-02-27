81c51d43a89872f01d6c4d00552dd4bb78cc01ba
just_adhenz <adhenz.miranda@hotmail.com>
2025-02-27 14:11:06 +0400

    added npms and scss code and htmls

--- frontend/js/components/navbar.js
+++ frontend/js/components/navbar.js
@@ -1,1 +1,10 @@
 // Controls navbar interactions.
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarToggle = document.getElementById('navbarToggle');
+    const navbarLinks = document.querySelector('.navbar-links');
+
+    navbarToggle.addEventListener('click', () => {
+        navbarToggle.classList.toggle('active');
+        navbarLinks.classList.toggle('active');
+    });
+});
