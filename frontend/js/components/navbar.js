fb1153f1e9758b8d7c681181891f2ac4d8c6c854
just_adhenz <adhenz.miranda@hotmail.com>
2025-03-10 11:39:42 +0400

    added the navbar

--- frontend/js/components/navbar.js
+++ frontend/js/components/navbar.js
@@ -1,10 +1,1 @@
 // Controls navbar interactions.
-document.addEventListener('DOMContentLoaded', () => {
-    const navbarToggle = document.getElementById('navbarToggle');
-    const navbarLinks = document.querySelector('.navbar-links');
-
-    navbarToggle.addEventListener('click', () => {
-        navbarToggle.classList.toggle('active');
-        navbarLinks.classList.toggle('active');
-    });
-});
