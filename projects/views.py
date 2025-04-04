67c357ab47e774f0dcbe027bc5a241d75f09d6d0
just_adhenz <adhenz.miranda@hotmail.com>
2025-04-04 16:18:18 +0400

    got projects working django style

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Projects 
 from .serializer import ProjectsSerializer
 from rest_framework.decorators import api_view
@@ -16,10 +16,29 @@ def contact(request):
     return render(request, 'contact.html')
 
 def projects_page(request):
-    return render(request, 'projects.html')
+    projects = Projects.objects.all()
+    # Group projects by year
+    projects_by_year = {}
+    for project in projects:
+        if project.year not in projects_by_year:
+            projects_by_year[project.year] = []
+        projects_by_year[project.year].append(project)
+    
+    # Sort years in descending order
+    sorted_years = sorted(projects_by_year.keys(), reverse=True)
+    
+    context = {
+        'projects_by_year': projects_by_year,
+        'sorted_years': sorted_years
+    }
+    return render(request, 'projects.html', context)
 
 def project_detail_page(request, id):
-    return render(request, 'project_detail.html')
+    project = get_object_or_404(Projects, id=id)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
 
 @api_view(['GET', 'POST'])
 def projects_list(request, format=None):
