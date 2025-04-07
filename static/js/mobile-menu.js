2e05b3622bbc8b2b8655c3960ac7b667735ad7ae
just_adhenz <adhenz.miranda@hotmail.com>
2025-04-07 19:25:06 +0400

    updated mobile-menu

--- static/js/mobile-menu.js
+++ static/js/mobile-menu.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.querySelector('.toggle-nav-btn');
     const navbarLinks = document.querySelector('.navbar-links');
+    const menuIcon = toggleButton.querySelector('img');
 
     toggleButton.addEventListener('click', function () {
         navbarLinks.classList.toggle('active');
+        toggleButton.classList.toggle('active');
+
+        if (navbarLinks.classList.contains('active')) {
+            menuIcon.src = "{% static 'assets/images/navbar/close.png' %}";
+        } else {
+            menuIcon.src = "{% static 'assets/images/navbar/menu.png' %}";
+        }
+    });
+
+    const navLinks = document.querySelectorAll('.navbar-links .nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navbarLinks.classList.remove('active');
+            toggleButton.classList.remove('active');
+            menuIcon.src = "{% static 'assets/images/navbar/menu.png' %}";
+        });
     });
 }); 
